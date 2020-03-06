67176617cd31acc7c7fb75dd00455c23bc39e1f1
Aleksei Beljajev <alekbeljak@gmail.com>
2020-03-06 13:03:43 +0200

    Refactoring carousel, new carousel first version

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import VisualizersCarousel from "../../features/VisualizersCarousel";
 import { observer } from "mobx-react-lite";
 
 const App = () => {
   return (
-    <div>
+    <Fragment>
       <VisualizersCarousel />
-    </div>
+      <div style={{width: "200px", height: "200px", backgroundColor: "blue"}}>
+      </div>
+    </Fragment>
   );
 };
 
